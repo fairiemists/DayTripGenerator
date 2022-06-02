4d9c67a11dff94d09a4e5b837786e987f0fc73f8
fairiemists <v.deweyness@gmail.com>
2022-06-01 18:54:19 -0700

    Working version - Removed recursives

--- main.py
+++ main.py
@@ -2,15 +2,8 @@ import random
 
 print()
 
-# Need Lists for DESTINATION / RESTAURANT / MODE OF TRANSPORTATION / 
-# FORM OF ENTERTAINMENT. 
 
-# Need random generator for each list. 
-
-# Would like to research restaurants/entertainment 
-# specific to each destination. 
-
-# Order of questions = entertainment/destination/transportation/restaurant
+# Would like to research restaurants/entertainment specific to each destination. 
 
 # add counter to say "there's only so many options" after 5 choices & loop back to choices ??
 
@@ -23,67 +16,57 @@ transportations = ["car", "motorcycle", "rollerskates", "horseback", "bicycle"]
 
 def choose_entertainment():
     random_entertainment = random.choice(entertainments)
-    user_input = input(f"Would you like to go {random_entertainment}? Select y/n for yes/no. ")
-    if user_input == "n":
-        print()
-        choose_entertainment()
-    elif user_input == "y":
-        print()
-        print(f"Great! Going {random_entertainment} is a great idea!")
-        return random_entertainment
-    else:
-        print()
-        print("I'm sorry. I didn't understand that. Let me try something else. ")
-        choose_entertainment()
+    user_input = ()
+    while user_input != "y": 
+        user_input = input(f"Would you like to go {random_entertainment}? Select y/n for yes/no. ")
+        if user_input == "y": 
+            print(f"Great! Going {random_entertainment} is a great idea!")
+            return random_entertainment
+        else: 
+            print("Let's try that again. ")
+            random_entertainment = random.choice(entertainments)    
 
 
 
 def choose_destination(selected_entertainment):
     random_destination = random.choice(destinations)
-    user_input = input(f"Would you like to go {selected_entertainment} in {random_destination}? Select y/n for yes/no. ")
-    if user_input == "n":
-        print()
-        choose_destination(selected_entertainment)
-    elif user_input == "y":
-        print()
-        print(f"{random_destination} is one of the best places to go {selected_entertainment}. ")
-        return random_destination
-    else: 
-        print()
-        print("I'm sorry. I didn't understand that. Let me try something else. ")
-        choose_destination(selected_entertainment)
+    user_input = ()
+    while user_input != "y":
+        user_input = input(f"Would you like to go {selected_entertainment} in {random_destination}? Select y/n for yes/no. ")
+        if user_input == "y":
+            print(f"{random_destination} is one of the best places to go {selected_entertainment}. ")
+            return random_destination
+        else: 
+            print("Let's try that again. ")
+            random_destination = random.choice(destinations)
+
 
 
 def choose_restaurant():
     random_restaurant = random.choice(restaurants)
-    user_input = input(f"Would you like to eat at {random_restaurant} restaurant on your trip? Select y/n for yes/no. ")
-    if user_input == "n":
-        print()
-        choose_restaurant()
-    elif user_input == "y":
-        print()
-        print(f"{random_restaurant} restaurant is one of my favorites. Good choice! ")
-        return random_restaurant
-    else: 
-        print()
-        print("I'm sorry. I didn't understand that. Let me try something else. ")
-        choose_restaurant()
+    user_input = ()
+    while user_input != "y":
+        user_input = input(f"Would you like to eat at {random_restaurant} restaurant on your trip? Select y/n for yes/no. ")
+        if user_input == "y":
+            print(f"{random_restaurant} restaurant is one of my favorites. Good choice! ")
+            return random_restaurant
+        else: 
+            print("Let's try that again. ")
+            random_restaurant = random.choice(restaurants)
 
 
 def choose_transportation():
     random_transportation = random.choice(transportations)
-    user_input = input(f"Would you like to get around by {random_transportation} during your trip? Select y/n for yes/no. ")
-    if user_input == "n": 
-        print()
-        choose_transportation()
-    elif user_input == "y":
-        print()
-        print(f"That sounds like a fun way to get around. ") 
-        return random_transportation
-    else: 
-        print()
-        print("I'm sorry. I didn't understand that. Let me try something else. ")
-        choose_transportation()
+    user_input = ()
+    while user_input != "y":
+        user_input = input(f"Would you like to get around by {random_transportation} during your trip? Select y/n for yes/no. ")
+        if user_input == "y":
+            print(f"That sounds like a fun way to get around. ")
+            return random_transportation
+        else:
+            print("Let's try that again. ")
+            random_transportation = random.choice(transportations)
+
 
 
 def planner():
@@ -97,21 +80,22 @@ def planner():
     selected_transportation = choose_transportation()
 
     print()
-    print(f"Let's see here. For your daytrip you have chosen to go {selected_entertainment} in {selected_destination} and eating at {selected_restaurant} restaurant. ")
+    print(f"Let's see here. For your daytrip you have chosen to go {selected_entertainment} in {selected_destination} then dining at {selected_restaurant} restaurant. ")
     print(f"You'll also be getting around by {selected_transportation} throughout the day. This sounds like fun! ")
     print()
-    user_input = input(f"Your daytrip looks complete. Would you like to lock in your choices? Select y/n for yes/no. ")
-    if user_input == "y":
-        print()
-        print(f"Yay! Congratulations! Enjoy the fun daytrip you have planned. ")
-    elif user_input == "n":
-        print()
-        print("That's okay. Let's start over. ")
-        planner()
-    else: 
-        print()
-        print("I'm sorry. I didn't understand that. ")
-        print()
+
+    user_input = ()
+    while user_input != "y":
+        user_input = input(f"Your daytrip looks complete. Would you like to lock in your choices? Select y/n for yes/no. ")
+        if user_input == "n":
+            print("That's okay. Let's start over. ")
+            planner()
+        if user_input == "y":
+            print("Yay! Congratulations! Enjoy the fun trip you have planned. ")
+            break
+        else:
+            print("I'm sorry. I didn't understand that. ")
+
 
 
 print("Having difficulty planning your weekend? I can help!")
